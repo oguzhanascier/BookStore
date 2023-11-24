bd3e69f7eeb4590edd1478d096d7d31c3c451963
oguzhanascier <oguzhanascier20@gmail.com>
2023-11-24 11:42:37 +0300

    Add project files.

---
+++ BookStore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("**   Türkçe Kitaplar Kategorisi    **    Yabancı Kitaplar Kategorisi    **");
+            Console.WriteLine();
+            Console.WriteLine("**   1-Çalıkuşu : Reşat Nuri       **    7-Tuna Kılavuzu: Jules Verne   **");
+            Console.WriteLine("**   2-Yaban : Yakup Kadri         **    8-Yabancı: Albet Camus         **");
+            Console.WriteLine("**   3-Tehlikeli Oyunlar: O.Atay   **    9-1987: George Orwell          **");
+            Console.WriteLine("**   4-Sinekli Bakkal : H.Edip     **    10-Arsene Lupin: M.Leblanc     **");
+            Console.WriteLine("**   5-Geçtiğm Günlerden:H.A.Yücel **    11-Altıncı Koğuz: A.Çehov      **");
+            Console.WriteLine("**   6-Kuyucaklı Yusuf : S.Ali     **    12-Kumarbaz: Dostoyevski       **");
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
